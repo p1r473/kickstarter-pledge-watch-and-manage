38a13cd12e9370ba101b79da987c4d1524ab0b38
p1r473 <subwayjared@gmail.com>
2014-10-28 05:32:47 -0400

    Update ks-watch-and-manage.py

--- ks-watch-and-manage.py
+++ ks-watch-and-manage.py
@@ -347,8 +347,8 @@ while True:
         logger.info('No limited rewards for this Kickstarter')
         sys.exit(0)
 
-	if ids:
-	    selected = [r for r in rewards if r[3] in ids]
+    #if ids:
+        #selected = [r for r in rewards if r[3] in ids]
     else:
         if pledges:
             selected = [r for r in rewards if r[0] in pledges]
